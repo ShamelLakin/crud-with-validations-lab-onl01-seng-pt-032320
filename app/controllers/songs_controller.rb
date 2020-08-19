d802e2239401036a235b225fd0b30bc2cdaaf19a
Shamel Lakin <christlife4u@gmail.com>
2020-08-19 15:18:32 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,6 +8,13 @@ class SongsController < ApplicationController
     end 
 
     def create 
+        @song = Song.new(post_params)
+        if @song.valid?
+           @song.save 
+        redirect_to song_path(@song)
+        else 
+            render :new
+        end 
     end 
 
     def show
@@ -17,4 +24,25 @@ class SongsController < ApplicationController
     def edit 
         @song = Song.find(params[:id])
     end 
+
+    def update
+        @song = Song.find(params[:id])
+      
+        @song.update(post_params)
+        # binding.pry
+        if @song.valid?
+          @song.save
+        
+        redirect_to song_path(@song)
+        else 
+          render :edit
+        end 
+       
+      end
+    
+      private
+    
+      def post_params
+        params.permit(:title, :released, :release_year, :artist_name, :genre)
+      end
 end
